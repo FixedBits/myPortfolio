818d088e0ab01d616a2efeb065fb77eff59aab35
Victor S. <fixingbitbybit@gmail.com>
2025-02-05 02:11:03 -0700

    Adding Dark-Mode

--- Mi Pieza/app.js
+++ Mi Pieza/app.js
@@ -5,7 +5,7 @@ document.addEventListener('contextmenu', function (event) {
 
 // Reduce video playback speed by half
 document.addEventListener('DOMContentLoaded', function () {
-  var video = document.getElementById('card-video');
+  const video = document.getElementById('card-video');
   if (video) {
     video.playbackRate = 0.5;
   }
@@ -13,3 +13,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // Set the text content of the element with id "year" to the current year
 document.getElementById('year').textContent = new Date().getFullYear();
+
+// Add an event listener to the button with the ID 'dark-mode'
+// When the button is clicked, toggle the 'dark-mode' class on the body element
+document.getElementById('dark-mode').addEventListener('click', function () {
+  document.body.classList.toggle('dark-mode');
+});
