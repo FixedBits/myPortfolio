5b66d202e84ed9c188267a0982af464b436dec0e
Victor S. <fixingbitbybit@gmail.com>
2025-03-30 05:13:29 -0600

    Updated toggle btn txt when clicked
    Used JS

--- Mi Pieza/app.js
+++ Mi Pieza/app.js
@@ -14,8 +14,13 @@ document.addEventListener('DOMContentLoaded', function () {
 // Set the text content of the element with id "year" to the current year
 document.getElementById('year').textContent = new Date().getFullYear();
 
-// Add an event listener to the button with the ID 'dark-mode'
-// When the button is clicked, toggle the 'dark-mode' class on the body element
 document.getElementById('dark-mode').addEventListener('click', function () {
   document.body.classList.toggle('dark-mode');
+
+  // Explicitly check the class and set the text to else
+  if (document.body.classList.contains('dark-mode')) {
+    this.textContent = 'Day Mode';
+  } else {
+    this.textContent = 'Dark Mode';
+  }
 });
